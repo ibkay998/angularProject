4b62b245febf8a2b60e49c00da0e4e84b0a870ca
ibukunolouwa oyeniyi <ibukun46@gmail.com>
2022-02-11 10:53:05 +0100

    added all the pages

--- src/app/module/admin/components/header/header.component.ts
+++ src/app/module/admin/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../../service/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService) {}
 
-  ngOnInit(): void {
+  ngOnInit( ): void {
+  }
+
+  logout(): void {
+    this.auth.logout();
   }
 
 }
